d260ced95a0526f3ebd30ff077775cbea22dbaf9
SavchukYuriy <owl1982@ukr.net>
2015-07-23 04:22:59 +0300

    First excercise Task03 is ready.

--- JavaBasicsCourse/src/com/luxoft/homeTasks/required/Task03.java
+++ JavaBasicsCourse/src/com/luxoft/homeTasks/required/Task03.java
@@ -10,7 +10,20 @@ package com.luxoft.homeTasks.required;
 public class Task03 {
 
 	public static void main(String[] args) {
-		
+		int a = 3;
+		int b = 5;
+		int c = 7;
+		int tmp;
+		System.out.println("Initial order: " + a + " " + b + " " + c);
+		tmp = c;
+		c = a;
+		a = b;
+		b = tmp;
+		System.out.println("Resulting order: " + a + " " + b + " " + c);
 	}
 
 }
+/*Result output: 
+ *  Initial order: 3 5 7
+ *  Resulting order: 5 7 3
+ */
