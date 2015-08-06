a1a7532aabbd1212bd72382bced12a6df25c02d9
SavchukYuriy <owl1982@ukr.net>
2015-08-07 02:54:41 +0300

    Messages translated to English

--- JavaBasicsCourse/src/com/luxoft/homeTasks/required/Task15.java
+++ JavaBasicsCourse/src/com/luxoft/homeTasks/required/Task15.java
@@ -5,23 +5,23 @@ public class Task15
     /*
      * @Author: Savchuk Yuriy
      * 
-     * 15.	Дано целое положительное число. Проверить истинность высказывания:
-     *  «Данное число является нечетным трехзначным»
+     * 15.	Дано целое положительное число. Проверить истинность высказывания: 
+     * «Данное число является нечетным трехзначным».
      */
     public static void main(String[] args)
     {
         int number = 687;
         if (number < 100) {
-            System.out.println("Ошибка. Число меньше 100: " + number); 
+            System.out.println("Error. Entered number has less than 3 symbols: " + number); 
         }
         else if (number > 999) {
-            System.out.println("Ошибка. Введено четырехзначное число: " + number); 
+            System.out.println("Error. Entered number has more than 3 symbols: " + number); 
         }
         else if (number%2 == 0){
-            System.out.println("Ошибка. Введено четное трехзначное число: " + number);
+            System.out.println("Error. Entered number is three-digit, but even: " + number);
         }
         else {
-            System.out.println("Данное число является нечетным трехзначным: " + number);
+            System.out.println("Bingo. This number is three-digit and odd: " + number);
         }
     }
 
